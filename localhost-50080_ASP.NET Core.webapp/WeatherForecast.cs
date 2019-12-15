11ce471b0d5350627c8a81c983b6ec2ff0e2626b
Nico Leputois <nicoleputois@outlook.com>
2019-12-15 14:01:18 +0100

    Ajoutez des fichiers projet.

---
+++ localhost-50080_ASP.NET Core.webapp/WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace localhost_50080_ASP.NET_Core.webapp
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
